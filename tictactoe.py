6830c089d9bfc09ae7c5f595c2caa5488054badb
Lance Rogers <lancekincaid1994@gmail.com>
2015-06-04 17:47:53 -0400

    getting started

---
+++ tictactoe.py
@@ -0,0 +1,41 @@
+import sys
+import math # math.factorial()
+
+
+def debug_print(information):
+    print("DEBUG: ", information, file=sys.stderr)
+
+# get your team & get your board
+team = input()
+first_row = input()
+second_row = input()
+third_row = input()
+
+# print out the inputs you're getting
+debug_print(team)
+debug_print(first_row)
+debug_print(second_row)
+debug_print(third_row)
+
+
+class Decisions:
+    # outputs the contents of my board
+    # board == incoming board plus my coordinates
+
+    def __init__(self):
+        self.team = input()
+        self.first_row = input()
+        self.second_row = input()
+        self.third_row = input
+
+
+    def get_team(self):
+        if self.input == 'X'
+            self.team == 'X'
+        else:
+            self.team == 'O'
+    def get_board(**args)
+        pass
+
+    def change_stratagy():
+        pass
