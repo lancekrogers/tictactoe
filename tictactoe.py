7244a39741fac1cd5c12c5a23fd14346ef249b8d
Lance Rogers <lancekincaid1994@gmail.com>
2015-06-05 18:28:18 -0400

    game works

--- tictactoe.py
+++ tictactoe.py
@@ -31,34 +31,59 @@ debug_print(third_row)
 #almost_board = [[first_row], [second_row], [third_row]]
 #board = np.array(almost_board)
 
-almost_board = first_row, second_row, third_row
+almost_board = [first_row], [second_row], [third_row]
 board = np.array(almost_board)
 
-def get_team(team):
-    my_bot_team = ''
-    if team == 'X':
-        my_bot_team = 'X'
-    else:
-        my_bot_team = 'O'
-    return my_bot_team
-my_bot_team = get_team(team)
 
-def change_stratagy(board, my_bot_team):
+
+
+def hierarchy(board, team):
     # logic for bot choice
-    if my_bot_team not in board:
-        if '_' == (board.flat[0][2]):
+    # functions as a hierarchy of move choices
+    A = (board.flat[0][2])
+    B = (board.flat[0][0])
+    C = (board.flat[2][0])
+    D = (board.flat[2][2])
+    E = (board.flat[0][1])
+    F = (board.flat[2][1])
+    G = (board.flat[1][0])
+    H = (board.flat[1][2])
+    I = (board.flat[1][1])
+
+
+    if team not in board:
+        # first game move
+        if '_' == A:
             print('0 2')
-        elif '_' == (board.flat[0][0]):
+        elif '_' == B:
             print('0 0')
-        elif '_' == (board.flat[2][0]):
+        elif '_' == C:
             print ('2 0')
-    elif my_bot_team in board:
-        if '_' == (board.flat[2][0]):
+        elif '_' == C:
             print('2 0')
-        elif '_' == (board.flat[2][2]):
+        elif '_' == D:
             print('2 2')
-        elif '_' == (board.flat[1][1]):
+        elif '_' == B:
+            print('0 0')
+        elif '_' == A:
+            print('0 2')
+        elif '_' == E:
+            print('0 1')
+        elif '_' == F:
+            print('2 1')
+        elif '_' == G:
+            print('1 0')
+        elif '_' == H:
+            print('1 2')
+        elif '_' == D:
+            print('2 2')
+        elif '_' == I:
             print('1 1')
+        elif '_' == F:
+            print('2 1')
+        elif '_' == G:
+            print('1 0')
+
 
 
 
@@ -66,7 +91,5 @@ def change_stratagy(board, my_bot_team):
 
 
 def main():
-    get_team(team)
-    my_bot_team = get_team(team)
-    change_stratagy(board, my_bot_team)
+    hierarchy(board, team)
 main()
