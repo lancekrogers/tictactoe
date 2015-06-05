ccd38292db90d1a858b61ff8b9652e8270265ab3
Lance Rogers <lancekincaid1994@gmail.com>
2015-06-05 16:02:46 -0400

    game is coming together

--- tictactoe.py
+++ tictactoe.py
@@ -31,35 +31,42 @@ debug_print(third_row)
 #almost_board = [[first_row], [second_row], [third_row]]
 #board = np.array(almost_board)
 
+almost_board = first_row, second_row, third_row
+board = np.array(almost_board)
 
-
-def get_team():
+def get_team(team):
     my_bot_team = ''
     if team == 'X':
         my_bot_team = 'X'
     else:
         my_bot_team = 'O'
-    print(my_bot_team)
     return my_bot_team
+my_bot_team = get_team(team)
 
-def get_board():
-    almost_board = first_row, second_row, third_row
-    board = np.array(almost_board)
-    return board
-
-def change_stratagy():
+def change_stratagy(board, my_bot_team):
     # logic for bot choice
     if my_bot_team not in board:
         if '_' == (board.flat[0][2]):
             print('0 2')
         elif '_' == (board.flat[0][0]):
             print('0 0')
-        elif '_' == (board.flat[][])
+        elif '_' == (board.flat[2][0]):
+            print ('2 0')
+    elif my_bot_team in board:
+        if '_' == (board.flat[2][0]):
+            print('2 0')
+        elif '_' == (board.flat[2][2]):
+            print('2 2')
+        elif '_' == (board.flat[1][1]):
+            print('1 1')
 
 
 
 
 
 
-get_team()
-get_board()
+def main():
+    get_team(team)
+    my_bot_team = get_team(team)
+    change_stratagy(board, my_bot_team)
+main()
