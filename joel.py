a9091a073e0105a1708303cc837d53a5e4fa38f3
Lance Rogers <lancekincaid1994@gmail.com>
2015-06-05 15:32:51 -0400

    I now have a logic stratagy

--- joel.py
+++ joel.py
@@ -11,17 +11,20 @@ team = input()
 first_row = input()
 second_row = input()
 third_row = input()
-almost_board = [[first_row], [second_row], [third_row]]
+almost_board = [first_row], [second_row], [third_row]
 board = np.array(almost_board)
 
 # print out the inputs you're getting
 debug_print(team)
-debug_print(first_row)
+debug_print(list(first_row))
 debug_print(second_row)
 debug_print(third_row)
-debug_print(almost_board)
+
 debug_print(board)
 
+debug_print((board.flat[0]))
+debug_print((board.flat[0])[1])
+debug_print((board.flat[0][1]))
 
 # Outputting the coordinates of your board
 import random
--- tictactoe.py
+++ tictactoe.py
@@ -24,25 +24,42 @@ debug_print(third_row)
     # outputs the contents of my board
     # board == incoming board plus my coordinates
 
-team = input()
-first_row = input()
-second_row = input()
-third_row = input()
-almost_board = [[first_row], [second_row], [third_row]]
-board = np.array(almost_board)
+#team = input()
+#first_row = input()
+#second_row = input()
+#third_row = input()
+#almost_board = [[first_row], [second_row], [third_row]]
+#board = np.array(almost_board)
 
 
 
-def get_team(team):
-    if team == 'X'
-        my_bot_team == 'X'
+def get_team():
+    my_bot_team = ''
+    if team == 'X':
+        my_bot_team = 'X'
     else:
-        my_bot_team == 'O'
+        my_bot_team = 'O'
+    print(my_bot_team)
+    return my_bot_team
 
-def get_board(first_row, second_row, third_row):
-    almost_board = [[first_row], [second_row], [third_row]]
+def get_board():
+    almost_board = first_row, second_row, third_row
     board = np.array(almost_board)
     return board
 
 def change_stratagy():
-    pass
+    # logic for bot choice
+    if my_bot_team not in board:
+        if '_' == (board.flat[0][2]):
+            print('0 2')
+        elif '_' == (board.flat[0][0]):
+            print('0 0')
+        elif '_' == (board.flat[][])
+
+
+
+
+
+
+get_team()
+get_board()
